feee3dbd72d95d651a049cff3c470429e13e6f49
Giangntph20619 <118795965+Giangntph20619@users.noreply.github.com>
2023-05-18 23:03:41 +0700

    alo

--- QuanAoTheThaoC5/Models/ColorDetails.cs
+++ QuanAoTheThaoC5/Models/ColorDetails.cs
@@ -2,5 +2,12 @@
 {
     public class ColorDetails
     {
+        public Guid Id { get; set; }
+        public Guid ProductID { get; set; }
+        public Guid ColorID { get; set; }
+        public string Description { get; set; }
+
+        // public virtual Product Product { get; set; } 
+        // public virtual Color Color { get; set; }
     }
 }
