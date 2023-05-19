232f16752cad754115a75d1c1c5789a6adb3565e
Dungndph20833 <118423397+honeybabyhhhhhh@users.noreply.github.com>
2023-05-19 18:50:58 +0700

    dung
    add cac bang

---
+++ QuanAoTheThaoC5/Models/CartDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace QuanAoTheThaoC5.Models
+{
+    public class CartDetails
+    {
+        public int Id { get; set; }
+        public Guid Product_ID { get; set; }
+        public Guid Cart_ID { get; set; }
+        public int Quantity { get; set; }
+        public virtual ICollection<Cart> Carts { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ QuanAoTheThaoC5/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace QuanAoTheThaoC5.Models
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
