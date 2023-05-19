c7577284ef26c2c2c9fa3ab36b2727316ef25bc6
Dungndph20833 <118423397+honeybabyhhhhhh@users.noreply.github.com>
2023-05-19 20:50:01 +0700

    Add  4 tables again
    none

---
+++ QuanAoTheThaoC5/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanAoTheThaoC5.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid User_ID { get; set; }
+        public int Status { get; set; }
+        public virtual User User { get; set; }
+    }
+}
