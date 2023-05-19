231b381a6c6707461e920dac3233b89c5117abf5
HuyDX131203 <huydxph20596@fpt.edu.vn>
2023-05-19 20:52:08 +0700

    add models

---
+++ QuanAoTheThaoC5/Models/Size.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanAoTheThaoC5.Controllers
+{
+    public class Size
+    {
+        public Guid SizeId { get; set; }
+        public string SizeName { get; set; }
+        public string Description { get; set; }
+        //public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
