431c8b6ef7d34ad1a2729c6c3a3c18e9cd59e96e
Xuân Khoa <vukhoa20122003@gmail.com>
2023-05-20 17:56:54 +0700

    đừng sửa erd của t , sửa t bắn đấy

--- QuanAoTheThaoC5/Configurations/ProductMaterialDetailsConfiguration.cs
+++ QuanAoTheThaoC5/Configurations/ProductMaterialDetailsConfiguration.cs
@@ -9,6 +9,8 @@ namespace QuanAoTheThaoC5.Configurations
         public void Configure(EntityTypeBuilder<ProductMaterialDetails> builder)
         {
             builder.HasKey(x => x.Id);
+            builder.Property(p => p.Description).HasColumnType("nvarchar(1000)").IsRequired();
+
             builder.HasOne(x => x.ProductMaterial).WithMany().HasForeignKey(x => x.IDProductMaterial);
             builder.HasOne(x => x.Product).WithMany().HasForeignKey(x => x.IDProduct);
         }
--- QuanAoTheThaoC5/Models/Size.cs
+++ QuanAoTheThaoC5/Models/Size.cs
@@ -5,6 +5,6 @@
         public Guid SizeId { get; set; }
         public string SizeName { get; set; }
         public string Description { get; set; }
-        public virtual IEnumerable<Product> Products { get; set; }
+      
     }
 }
