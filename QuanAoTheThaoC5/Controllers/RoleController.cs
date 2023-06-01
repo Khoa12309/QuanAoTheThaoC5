32bd868ffefc691fea4a8a4920982c62cec4c3fa
Xuân Khoa <vukhoa20122003@gmail.com>
2023-06-01 15:01:43 +0700

    cru api web

--- QuanAoTheThaoC5/Controllers/RoleController.cs
+++ QuanAoTheThaoC5/Controllers/RoleController.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using QuanAoTheThaoC5.Models;
-
-
+using System.Diagnostics;
+using System.Text;
 
 namespace QuanAoTheThaoC5.Controllers
 {
 	public class RoleController : Controller
 	{
-		
+        
 		public IActionResult Index()
 		{
 			return View();
@@ -25,24 +25,58 @@ namespace QuanAoTheThaoC5.Controllers
             List<Role> result = JsonConvert.DeserializeObject<List<Role>>(apiData);
             return View(result);
 		}
-		public async Task<IActionResult> CreateRole()
+		[HttpGet]
+		public IActionResult CreateRole()
 		{
 			return View();
 		}
 		[HttpPost]
-        public async Task<IActionResult> CreateRole(string name)
+        public async Task<IActionResult> CreateRole(Role obj)
 		{
-			string requestURL =
-			$"https://localhost:7001/api/Role/" + $"CreateRole?Name={name}";
+			string data = JsonConvert.SerializeObject(obj);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            string requestURL =
+			$"https://localhost:7001/api/Role" ;
 			var httpClient = new HttpClient(); // Tại 1 httpClient để call API
-			var response = await httpClient.GetAsync(requestURL); // Lấy kết quả
-																  // Đọc ra string Json
+			var response = await httpClient.PostAsync(requestURL+ "/CreateRole", content); // Lấy kết quả// ]Đọc ra string Json
 			string apiData = await response.Content.ReadAsStringAsync();
 			// Lấy kết quả thu được bằng cách bóc dữ liệu Json
-			List<Role> result = JsonConvert.DeserializeObject<List<Role>>(apiData);
 
 			return RedirectToAction("RoleView");
-
         }
-	}
+		[HttpGet]
+        public IActionResult UpdateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(Role obj)
+        {
+            string data = JsonConvert.SerializeObject(obj);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            string requestURL =
+            $"https://localhost:7001/api/Role";
+            var httpClient = new HttpClient(); // Tại 1 httpClient để call API
+            var response = await httpClient.PutAsync(requestURL + "/UpdateRole", content); 
+
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kết quả thu được bằng cách bóc dữ liệu Json
+            return RedirectToAction("RoleView");
+        }
+      
+        public async Task<IActionResult> DeleteRole(Guid id)
+        {
+
+            string requestURL =
+            $"https://localhost:7001/api/Role";
+            var httpClient = new HttpClient(); // Tại 1 httpClient để call API
+            var response = await httpClient.DeleteAsync(requestURL + "/DeleteRole/"+id); 
+
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kết quả thu được bằng cách bóc dữ liệu Json
+            return RedirectToAction("RoleView");
+        }
+    }
 }
